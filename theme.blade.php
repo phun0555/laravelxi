e3d1a21bb53190e0baf94c4d18e09045b19a857e
phun0555 <152048536+phun0555@users.noreply.github.com>
2024-12-08 13:38:57 +0700

    Add files via upload

---
+++ theme.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+</head>
+<body>
+    <header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+       
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+    <h1 class= "display"
+    <div class="container">    
+        <div class="text-center">
+            @yield("title")
+        </div>    
+
+                
+    
+        
+            
+ 
+    <footer class= "pt-4 my-md-5 pt-md-5 border-top">
+    </div>
+</body>
+
+</html>
